4faeaa072628fb6804efa5e4c1e70a8ba75d526a
James Huang <jhu9000@gmail.com>
2023-08-28 14:57:22 -0700

    update

--- src/EventSubscriber/EventSubscriber.php
+++ src/EventSubscriber/EventSubscriber.php
@@ -7,7 +7,7 @@ use Drupal\feeds\Event\FeedsEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Modifies the parsed result of a feed.
+ * Feeds event subscriber.
  */
 class EventSubscriber implements EventSubscriberInterface {
 
@@ -15,32 +15,28 @@ class EventSubscriber implements EventSubscriberInterface {
    * {@inheritdoc}
    */
   public static function getSubscribedEvents() {
-    $events[FeedsEvents::PROCESS_ENTITY_PRESAVE][] = 'entityPresave';
+    $events[FeedsEvents::PROCESS_ENTITY_PREVALIDATE][] = 'entityPrevalidate';
     return $events;
   }
 
   /**
-   * Finds paragraphs and sets them to node paragraph fields by matching the
-   * node feeds item guid with the paragraph feeds item guid.
+   * Callback for feeds entity prevalidate event.
    *
    * @param Drupal\feeds\Event\EntityEvent $event
    */
-  public function entityPresave(EntityEvent $event) {
+  public function entityPrevalidate(EntityEvent $event) {
     $entity = $event->getEntity();
     $guid = @$entity->feeds_item[0]->guid;
     if ($guid) {
-      $database = \Drupal::database();
-      $entity_field_manager = \Drupal::service('entity_field.manager');
-      $entity_type = $entity->getEntityTypeId();
-      $bundle = $entity->bundle();
       /** \Drupal\Core\Field\FieldDefinitionInterface */
-      $fields = $entity_field_manager->getFieldDefinitions($entity_type, $bundle);
+      $fields = $entity->getFieldDefinitions($entity_type, $bundle);
       foreach ($fields as $name => $field) {
         /** \Drupal\Core\Field\FieldStorageDefinitionInterface */
         $field_storage = $field->getFieldStorageDefinition();
-        $type =  $field_storage->getType();
+        $type = $field_storage->getType();
         $target_type = $field_storage->getSetting('target_type');
         if ($type == 'entity_reference_revisions' && $target_type == 'paragraph') {
+          $database = \Drupal::database();
           $query = $database->query("
             SELECT feeds_item_guid, entity_id, revision_id
             FROM {paragraph__feeds_item}
@@ -51,21 +47,22 @@ class EventSubscriber implements EventSubscriberInterface {
           ]);
           $result = $query->fetchAll();
           if ($result) {
-            $paragraphs = [];
+            $items = [];
             foreach ($result as $record) {
-              // Unfortunately, the db query above is written to be compatible
-              // with various databases and can return extra results of nested
-              // paragraphs, so this secondary check using regex is required
-              // to filter on only the direct child.
-              if (preg_match('/^'.$guid.'-'.$name.'-[0-9]+$/', $record->feeds_item_guid)) {
-                $paragraphs[] = [
+              // The db query above is written to avoid using database specific
+              // syntax and will return results for all descendents. This regex
+              // filters for only the direct children.
+              if (preg_match('/^'.$guid.'-'.$name.'-([0-9])+$/', $record->feeds_item_guid, $matches)) {
+                $delta = $matches[1];
+                $items[$delta] = [
                   'target_id' => $record->entity_id,
                   'target_revision_id' => $record->revision_id
                 ];
               }
             }
-            if (!empty($paragraphs)) {
-              $entity->set($name, $paragraphs);
+            if (!empty($items)) {
+              ksort($items);
+              $entity->set($name, $items);
             }
           }
         }
